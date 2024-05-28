d3ce2c7570e797aee3ccb5f530de6cb6fadbbac4
zlima410 <endercraft68@gmail.com>
2024-05-27 23:20:49 -0400

    Add another test to test_textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,9 +4,13 @@ from textnode import TextNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", "bold", "htts://www.google.com")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
+
+        node3 = TextNode("This is a tex node", "bold", "htts://www.google.com")
+        node4 = TextNode("This is a text nod", "bold", "htts://www.google.com")
+        self.assertEqual(node3, node4)
 
 
 if __name__ == "__main__":
