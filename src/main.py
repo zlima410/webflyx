0ade84c318881e538769435ce72a012755aeb6a5
zlima410 <endercraft68@gmail.com>
2024-06-03 20:15:21 -0400

    Add function to make a TextNode into an HTMLNode and test it in main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-from textnode import TextNode
+from textnode import TextNode, text_node_to_html_node
 
 if __name__ == "__main__":
     text_node = TextNode("This is a text node", "bold", "http://www.boot.dev")
     print(text_node)
+
+    print(text_node_to_html_node(text_node))
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import LeafNode
 class TextNode():
     def __init__(self, text, text_type, url):
         self.text = text
@@ -13,3 +14,19 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+        if text_node.text_type == "text":
+            return LeafNode(value=text_node.text)
+        elif text_node.text_type == "bold":
+            return LeafNode(tag="b", value=text_node.text)
+        elif text_node.text_type == "italic":
+            return LeafNode(tag="i", value=text_node.text)
+        elif text_node.text_type == "code":
+            return LeafNode(tag="code", value=text_node.text)
+        elif text_node.text_type == "link":
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        elif text_node.text_type == "image":
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+        else:
+            raise ValueError(f"Invalid text type: {text_node.text_type}")
