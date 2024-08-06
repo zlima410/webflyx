fcc23f307dafa19019bb76bc4dd40c7fbecd0d71
zlima <endercraft68@gmail.com>
2024-08-06 16:49:21 -0500

    Used regex to create function to distinguish block type

--- src/markdown_processor.py
+++ src/markdown_processor.py
@@ -7,6 +7,7 @@ from textnode import (
     text_type_italic,
     text_type_code,
 )
+import re
 
 def text_to_text_nodes(text):
     text_nodes = [TextNode(text, "text")]
@@ -27,3 +28,33 @@ def markdown_to_blocks(markdown):
         if len(new_block) > 0:
             new_blocks.append(new_block)
     return new_blocks
+
+def block_to_block_type(block):
+    lines = block.strip().split("\n")
+
+    if re.match(r"^(#{1,6})\s", lines[0]):
+        return "heading"
+    elif block.startswith("```") and block.endswith("```"):
+        return "code"
+    elif re.match(r"^(>\s.*\n?)+", block):
+        return "quote"
+    
+    unordered_list_regex = r"^\s*[-*]\s+.*"
+    if all(re.match(unordered_list_regex, line) for line in lines):
+        return "unordered_list"
+    
+    ordered_list_regex = r"^(\d+)\.\s.*"
+    previous_number = 0
+    for line in lines:
+        match = re.match(ordered_list_regex, line)
+        if match:
+            current_number = int(match.group(1))
+            if current_number != previous_number + 1:
+                return "paragraph"
+            previous_number = current_number
+        else:
+            return "paragraph"
+    if previous_number > 0:
+        return "ordered_list"
+    
+    return "paragraph"
